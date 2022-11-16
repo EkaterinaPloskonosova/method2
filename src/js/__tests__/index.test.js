f3e9b07e65a3d9d1513c70f8d99764ec960d771f
Ekaterina Ploskonosova <ploskev@yahoo.com>
2022-11-16 10:12:01 +0500

    commit

--- src/js/__tests__/index.test.js
+++ src/js/__tests__/index.test.js
@@ -9,7 +9,10 @@ import Zombie from '../zombie.js';
 test('checking for class output without errors', () => {
   const result = new Character('bowman1', 'Bowman');
   const expected = {
-    name: 'bowman1', type: 'Bowman', health: 100, level: 1,
+    name: 'bowman1',
+    type: 'Bowman',
+    health: 100,
+    level: 1,
   };
   expect(result).toEqual(expected);
 });
@@ -29,7 +32,12 @@ test('check, error by class name', () => {
 test('checking class output Bowman', () => {
   const result = new Bowman('bowman1', 'Bowman');
   const expected = {
-    name: 'bowman1', type: 'Bowman', health: 100, level: 1, attack: 25, defence: 25,
+    name: 'bowman1',
+    type: 'Bowman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
   };
   expect(result).toEqual(expected);
 });
@@ -37,7 +45,12 @@ test('checking class output Bowman', () => {
 test('checking class output Swordsman', () => {
   const result = new Swordsman('swordsman1', 'Swordsman');
   const expected = {
-    name: 'swordsman1', type: 'Swordsman', health: 100, level: 1, attack: 40, defence: 10,
+    name: 'swordsman1',
+    type: 'Swordsman',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
   };
   expect(result).toEqual(expected);
 });
@@ -45,7 +58,12 @@ test('checking class output Swordsman', () => {
 test('checking class output Magician', () => {
   const result = new Magician('magician1', 'Magician');
   const expected = {
-    name: 'magician1', type: 'Magician', health: 100, level: 1, attack: 10, defence: 40,
+    name: 'magician1',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
   };
   expect(result).toEqual(expected);
 });
@@ -53,7 +71,12 @@ test('checking class output Magician', () => {
 test('checking class output Daemon', () => {
   const result = new Daemon('daemon1', 'Daemon');
   const expected = {
-    name: 'daemon1', type: 'Daemon', health: 100, level: 1, attack: 10, defence: 40,
+    name: 'daemon1',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defence: 40,
   };
   expect(result).toEqual(expected);
 });
@@ -61,7 +84,12 @@ test('checking class output Daemon', () => {
 test('checking class output Undead', () => {
   const result = new Undead('undead1', 'Undead');
   const expected = {
-    name: 'undead1', type: 'Undead', health: 100, level: 1, attack: 25, defence: 25,
+    name: 'undead1',
+    type: 'Undead',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defence: 25,
   };
   expect(result).toEqual(expected);
 });
@@ -69,7 +97,62 @@ test('checking class output Undead', () => {
 test('checking class output Zombie', () => {
   const result = new Zombie('zombie1', 'Zombie');
   const expected = {
-    name: 'zombie1', type: 'Zombie', health: 100, level: 1, attack: 40, defence: 10,
+    name: 'zombie1',
+    type: 'Zombie',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defence: 10,
   };
   expect(result).toEqual(expected);
+});
+
+test('test levelUp health > 0 ', () => {
+  const character3 = new Character('bowman1', 'Bowman');
+  character3.attack = 40;
+  character3.defence = 20;
+  character3.levelUp();
+  const standard = {
+    name: 'bowman1',
+    type: 'Bowman',
+    health: 100,
+    level: 2,
+    attack: 48,
+    defence: 24,
+  };
+  expect(character3).toEqual(standard);
+});
+
+test('test levelUp error', () => {
+  expect(() => {
+    const character3 = new Character('bowman1', 'Bowman');
+    character3.health = -1;
+    character3.levelUp();
+    return character3;
+  }).toThrow(Error);
+});
+
+test('test for correct damage method', () => {
+  const character3 = new Character('bowman1', 'Bowman');
+  character3.attack = 40;
+  character3.defence = 20;
+  character3.damage(10);
+  const standard = {
+    name: 'bowman1',
+    type: 'Bowman',
+    health: 92,
+    level: 1,
+    attack: 40,
+    defence: 20,
+  };
+  expect(character3).toEqual(standard);
+});
+
+test('test for damage method with fatal health level', () => {
+  expect(() => {
+    const character3 = new Character('bowman1', 'Bowman');
+    character3.health = -1;
+    character3.damage(10);
+    return character3;
+  }).toThrow(new Error('Персонаж погиб!'));
 });
--- src/js/character.js
+++ src/js/character.js
@@ -14,4 +14,23 @@ export default class Character {
       throw new Error('Такого типа персонажа не существует.');
     }
   }
+
+  levelUp() {
+    if (this.health > 0) {
+      this.level += 1;
+      this.health = 100;
+      this.attack *= 1.2;
+      this.defence *= 1.2;
+    } else {
+      throw new Error('Нельзя повысить левел умершего!');
+    }
+  }
+
+  damage(points) {
+    if (this.health >= 0) {
+      this.health -= points * (1 - this.defence / 100);
+    } else {
+      throw new Error('Персонаж погиб!');
+    }
+  }
 }
